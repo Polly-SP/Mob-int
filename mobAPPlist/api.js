ed698f508fbc58aa080e5d439556c32ded298ef7
Polly-SP <61331991+Polly-SP@users.noreply.github.com>
2021-01-28 17:17:08 +0300

    Update api.js

--- mobAPPlist/api.js
+++ mobAPPlist/api.js
@@ -1,15 +1,14 @@
 import Axios from "axios";
 
-// класс со статическими полями с такими как HTTP (HTTP - клиент, обозначенным для него baseURL, то есть с каким Юрл мы обращаемся ко всем нашим рутам)
+
 export class API {
   static http = Axios.create({
     baseURL: "http://api.openWinerydb.org/",
   });
-  //метод getWineries возвращает ПромисГет, то есть будет происходить ГЕТ запрос с такими "Wineries" урлом
+  //метод getWineries 
   static getWineries() {
     return API.http.get("Wineries");
   }
-  //здесь спрашивает только одну винодельню
   static getWinery(id) {
     return API.http.get(`Wineries/${id}`);
   }
