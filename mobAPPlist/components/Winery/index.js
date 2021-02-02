4ff1c72e038f8974a540696abe9184a81461d39e
Polly-SP <61331991+Polly-SP@users.noreply.github.com>
2021-02-02 11:25:50 +0300

    Update index.js

--- mobAPPlist/components/Winery/index.js
+++ mobAPPlist/components/Winery/index.js
@@ -18,23 +18,23 @@ import { useParams } from "react-router-native";
 
 import { API } from "../../api";
 
-// все тоже самое, только здесь берется ID рута (позволяет извлекать параметры рута через useParams)
+
 export const WineryDetails = () => {
   const { id } = useParams();
   const { isLoading, error, data } = useQuery(id, () => API.getWinery(id));
   if (isLoading) return <H2>Loading...</H2>;
   if (error || !data.data)
     return <Text>An error has occurred: {error.message}</Text>;
-    // кнопка позвонить
+    
   const onCall = () => {
     Linking.openURL("tel:" + data.data.phone);
   };
-  //перейти на веб сайт
+  
   const onGoToWebsite = () => {
     Linking.openURL(data.data.website_url);
   };
   return (
-    // название винодельни, штат, тип , кнопки позвонить и сайт
+    
     <Content>
       <H2>{data.data.name}</H2>
       <H3>
